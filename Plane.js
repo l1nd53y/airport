558114656652b8e206ef3660c3f000cdd77a3a64
Lindsey Trujillo <lindsey.trujillo@verizon.com>
2021-06-30 17:14:59 -0500

    Progress

--- Plane.js
+++ Plane.js
@@ -1,8 +1,19 @@
 class Plane {
+
+    flightNumber;
+    passengers;
+    origin;
+    destination;
+    crew;
+
     constructor(flightNumber, airline) {
         this.flightNumber = flightNumber;
         this.airline = airline;
     }
+
+    /*boardPassenger(passenger){
+        this.passengers.push(passenger)
+    }*/
 }
 
 module.exports = Plane
--- Plane.test.js
+++ Plane.test.js
@@ -1,8 +1,20 @@
-const Plane = require('./plane');
+const Plane = require('./Plane');
+const Passenger = require('./Passenger');
 
 describe('Plane', () => {
     test('Constructs plane with flight number & airline', () => {
         const LA727 = new Plane('LA727', 'Delta');
         expect(LA727.flightNumber).toBe('LA727', 'Delta');
     })
-});
+
+
+/* const lindsey = new Passenger("Lindsey")
+const thomas = new Passenger("Thomas")
+
+    test('Plane has passengers', ()=>{
+        const exPlane = new Plane('LA727', 'Delta');
+        exPlane.boardPassenger(lindsey);
+        exPlane.boardPassenger(thomas);
+        expect(exPlane.passengers.length).toBe(2);
+    })*/
+}); 
